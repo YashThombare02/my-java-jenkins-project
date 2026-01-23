4b7b12fba640f6bf5c4769626155b4af924bdf6c
Yash Thombare <ythombare1972@gmail.com>
2026-01-23 09:07:31 +0530

    Change expected result in testAdd to 10
    Updated expected value in testAdd method to 10.

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -6,6 +6,7 @@ public class HelloTest {
     @Test
     public void testAdd() {
         Hello h = new Hello();
-        assertEquals(5, h.add(2, 3));
+        assertEquals(10, h.add(2, 3));   // intentionally wrong
     }
 }
+
